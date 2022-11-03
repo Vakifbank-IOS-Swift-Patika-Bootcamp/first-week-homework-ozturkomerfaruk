6c7333e695f3d9cd0e2ef7d10eb1417bc5e59e7f
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2022-11-03 18:28:39 +0300

    question5-1

--- vakifbankIosBootcamp.playground/Contents.swift
+++ vakifbankIosBootcamp.playground/Contents.swift
@@ -95,3 +95,25 @@ func Question4(numberOfRows: Int) {
 }
 
 Question4(numberOfRows: 5)
+
+
+print("------------------------------Soru 5 - Project Euler 1------------------------------")
+
+/*
+ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ 
+ Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+func Question5A(num: Int) -> String {
+    var sum = 0
+    for i in 1..<num {
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i
+        }
+    }
+    
+    return String(describing: sum)
+}
+
+print("Sum: \(Question5A(num: 1000))")
